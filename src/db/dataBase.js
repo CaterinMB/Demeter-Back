e15dc477002002dd7fbee0cb2b220f207e1d4309
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-15 20:37:23 -0500

    DB

---
+++ src/db/dataBase.js
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize';
+import { DB_Host, DB_User, DB_Password, DB_Name } from '../config.js'
+
+export const sequelize = new Sequelize(
+  'demeter',
+  'root',
+  '',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
