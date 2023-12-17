8aaaf33e44574a9b1ddba65325f0dee55b019ae9
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-16 21:20:13 -0500

    Basico

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ export const TOKEN_SECRET = 'some secret key'
 export const port = process.env.port || 5080
 
 export const DB_Host = process.env.DB_Host || 'localhost'
-export const DB_Port = process.env.DB_Port || 'localhost'
+export const DB_Port = process.env.DB_Port || '5080'
 export const DB_User = process.env.DB_User || 'root'
 export const DB_Password = process.env.DB_Password || ""
 export const DB_Name = process.env.DB_Name || 'demeter'
