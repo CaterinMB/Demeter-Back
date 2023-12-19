2e2480ec02d812ed64848618a347acac987dd627
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-19 00:08:30 -0500

    ip

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import app from './app.js'
 import { sequelize } from './db/dataBase.js'
 
 async function main() {
-    try{
-        await sequelize.sync({force: false})
-        app.listen(5080);
-        console.log('Server on port ', 5080);
+    try {
+        await sequelize.sync({ force: false })
+        app.listen(5080, '10.10.10.202', () => {
+            console.log('Server is running at http://10.10.10.202:5080');
+        });
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
