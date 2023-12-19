e0e9651c6ce378589d4bf33629320794844c1391
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-18 22:16:04 -0500

    package

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import app from './app.js'
 import { sequelize } from './db/dataBase.js'
-const port = process.env.PORT || 5080
 
 async function main() {
     try{
         await sequelize.sync({force: false})
-        app.listen(port);
-        console.log('Server on port ', port);
+        app.listen(5080);
+        console.log('Server on port ', 5080);
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
