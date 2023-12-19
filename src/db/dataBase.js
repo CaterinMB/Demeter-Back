903d1df4fe692d0779914cb17ea18913bb65c174
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-18 22:34:00 -0500

    data

--- src/db/dataBase.js
+++ src/db/dataBase.js
@@ -1,24 +1,11 @@
 import { Sequelize } from 'sequelize';
 
-// const dbConfig = {
-//   host: process.env.DB_Host || 'localhost',
-//   port: process.env.DB_Port || '3306',
-//   username: process.env.DB_User || 'root',
-//   password: process.env.DB_Password || '',
-//   database: process.env.DB_Name || 'demeter',
-//   dialect: 'mysql' 
-// }
-
-// export const sequelize = new Sequelize(
-//   dbConfig
-// );
-
 export const sequelize = new Sequelize(
   'demeter',
   'root',
   '',
   {
-    host: 'localhost',
+    host: '10.10.10.202',
     dialect: 'mysql' 
   }
 );
